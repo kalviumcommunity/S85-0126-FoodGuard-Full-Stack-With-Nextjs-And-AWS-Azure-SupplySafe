899bd6178852e6bef9b016e35b1bbf943ba1d278
Madhav Garg <madhavgarg3300@gmail.com>
2026-01-19 13:51:48 +0530

    madhav docker setup

--- supplysafe/eslint.config.mjs
+++ supplysafe/eslint.config.mjs
@@ -1,18 +1,28 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-plugin-prettier";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+export default [
+  js.configs.recommended,
 
-export default eslintConfig;
+  // TypeScript recommended rules
+  ...tseslint.configs.recommended,
+
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      prettier,
+    },
+    rules: {
+      "no-console": "warn",
+      "prettier/prettier": "error",
+      "quotes": ["error", "double"],
+      "semi": ["error", "always"],
+    },
+  },
+];
